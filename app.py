98988408e69f7a34bc6b0098518f8af74af22732
nadineassyra <nadineassyra1304@gmail.com>
2026-04-24 15:26:28 +0700

    Dashboard Streamlit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from data import *
 
 def judul(): 
 # Judul Dashboard
-    st.title("📊 Dashboard COVID-19")
+    st.title("😷 Dashboard COVID-19")
     st.write("Selamat datang di dashboard interaktif untuk menganalisis data COVID-19 di Indonesia")
 
 st.sidebar.title("Navigasi")
@@ -11,9 +11,23 @@ menu = st.sidebar.radio("Pilih halaman", ["Home", "Halaman Data"])
 
 if menu == "Home":
     judul()
+    # Pilih tahun
+    year = select_year()
+    # Load & filter data
+    df = load_data()
+    df_filtered = filter_data(df, year)
+    kolom(df_filtered)
+    pie_chart1(df_filtered)
+    
     # Copyright
     st.write("© Nadine Assyra - NPM: 12345678")
+    
 elif menu == "Halaman Data":
     judul()
-    show_data()
+    year = select_year()
+    df = load_data()
+    df_filtered = filter_data(df, year)
+    show_data(df_filtered)
+    
+    st.write("© Nadine Assyra - NPM: 12345678")
 
--- data.py
+++ data.py
@@ -1,32 +1,79 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 # Fungsi untuk memuat data
 def load_data():
     df = pd.read_csv("dataset\covid_19_indonesia_time_series_all.csv")
     return df
 
-# Fungsi untuk menampilkan data dalam bentuk tabel
-def show_data():
-    df = load_data()
-    st.subheader("📌 Data COVID-19 Indonesia")
-    st.dataframe(df.head(10)) # Menampilkan 10 data pertama
-    
-# Menampilkan statistik deskriptif dataset
-    st.subheader("📊 Statistik Deskriptif Dataset")
-    st.write(df.describe()) # Menampilan statistik deskriptif
-    
-# Total kasus keseluruhan
-    total_cases = df["Total Cases"].sum()
-    st.subheader("Total Kasus Keseluruhan")
-    st.write(total_cases)
-    
-# Kolom location sampai total recovery
-    st.subheader("Data Kolom Tertentu")
-    data_kolom = df.loc[:, "Location":"Total Recovered"]
-    st.dataframe(data_kolom)
+def filter_data(df, year=None):
+    if year:
+        df = df[df['Date'].astype(str).str.contains(str(year))]
+    return df
 
-# Copyright
-    st.write("© Nadine Assyra - NPM: 12345678")
+def select_year():
+    return st.sidebar.selectbox(
+        "Pilih Tahun 📅",
+        options=[None, 2020, 2021, 2022],
+        format_func=lambda x: "Semua Tahun" if x is None else str(x)
+    )
+
+# Fungsi untuk menampilkan data dalam bentuk tabel
+def show_data(df):
+    selected_columns = ['Location'] + list(df.loc[:, 'New Cases':'Total Recovered'].columns)
+    df_selected = df[selected_columns]
+    st.subheader("Data COVID-19 Indonesia 🔴⚪")
+    st.dataframe(df_selected.head(10)) # Menampilkan 10 data pertama
+
+# Total kasus
+def total_case(df):
+    df = load_data()
+    total_kasus = df['New Cases'].sum()
+    return total_kasus
+
+# Total Death
+def total_death(df):
+    df = load_data()
+    total_kematian = df['New Deaths'].sum()
+    return total_kematian
+    
+# Total_sembuh
+def total_recovery(df):
+    df = load_data()
+    total_sembuh = df['New Recovered'].sum()
+    return total_sembuh
+
+# Kolom 1
+def kolom(df):
+    kasus = total_case(df)
+    kematian = total_death(df)
+    sembuh = total_recovery(df)
+    
+    co11, co12, co13 = st.columns(3)
+    co11.metric(label="Total Kasus 📈", value=kasus, border=True)
+    co12.metric(label="Total Kematian 💀", value=kematian, border=True)
+    co13.metric(label="Total Sembuh 🏋️‍♀️", value=sembuh, border=True)
+
+# Piechart1
+def pie_chart1(df):
+    total_mati = total_death(df)
+    total_sumbuh = total_recovery(df)
+    
+    data ={
+        'Status' : ['Meninggal', 'Sembuh'],
+        'Jumlah' : [total_mati,total_sumbuh]
+        }
+        
+    fig = px.pie(   
+        data,
+        names='Status',
+        values='Jumlah',
+        title='Perbandingan Total Kematian VS Total Kesembuhan',
+        hole=0.5,
+        color_discrete_sequence=['#4de89f', '#ff6459']
+    )
+    
+    st.plotly_chart(fig, use_container_width=True)
     
     
